cab80d78314c92e62b6d429e4ec120f8e1d5ef66
GaYoung <kians2@naver.com>
2020-12-29 02:17:13 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const {sequelize} = require('./models');
+const indexRouter = require('./routes/index');
+
+sequelize.sync({ alter : false})
+.then(() => {
+  console.log('데이터베이스 연결 성공');
+})
+.catch((error) => {
+  console.error(error);
+})
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use('/', indexRouter);
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+const port = 3002;
+app.listen(port, ()=> console.log(`app listening on port ${port}!`));
---
+++ modules/responseMessage.js
@@ -0,0 +1,36 @@
+module.exports = {
+    NULL_VALUE: "필요한 값이 없습니다.",
+    OUT_OF_VALUE: "파라미터 값이 잘못 되었습니다.",
+  
+    /* 토큰 */
+    EMPTY_TOKEN: '토큰 값이 없습니다.',
+    EXPIRED_TOKEN: '토큰 값이 만료되었습니다.',
+    INVALID_TOKEN: '유효하지 않은 토큰값입니다.',
+    AUTH_SUCCESS: '인증에 성공했습니다.',
+    ISSUE_SUCCESS: '새로운 토큰이 생성되었습니다.',
+  
+    /* User */
+    READ_USER_SUCCESS: "사용자 조회 성공",
+    READ_USER_ALL_SUCCESS: "전체 사용자 조회 실패",
+    READ_USER_FAIL: "사용자 조회 성공",
+    READ_USER_ALL_FAIL: "전체 사용자 조회 실패",
+    UPDATE_USER_SUCCESS: "사용자 업데이트 성공",
+    UPDATE_USER_FAIL: "사용자 업데이트 실패",
+    DELETE_USER_SUCCESS: "사용자 삭제 성공",
+    DELETE_USER_FAIL: "사용자 삭제 실패",
+  
+    /* 회원가입 */
+    SIGN_UP_SUCCESS: "회원 가입 성공.",
+    SIGN_UP_FAIL: "회원 가입 실패.",
+    SIGN_IN_SUCCESS: "로그인 성공.",
+    SIGN_IN_FAIL: "로그인 실패.",
+    ALREADY_ID: "존재하는 ID 입니다.",
+    NO_USER: "존재하지않는 유저 id 입니다.",
+    ALREADY_EMAIL: "이미 존재하는 이메일 입니다.",
+    NO_EMAIL: '존재하지 않는 이메일 입니다.',
+    MISS_MATCH_PW: "비밀번호가 일치하지 않습니다",
+  
+    /* 서버에러 */
+    INTERNAL_SERVER_ERROR: "서버 내부 오류",
+  }
+  
