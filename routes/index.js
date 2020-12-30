d332c2f5024cebcb8fbb2962860eb9585a746277
GaYoung <kians2@naver.com>
2020-12-30 15:00:35 +0900

    1-createRoute 기본 라우터 파일 생성 완료 close #1

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/home', require('./home'));
+router.use('/answers', require('./answers'));
+router.use('/friends', require('./friends'));
+router.use('/users', require('./users'));
+router.use('/exploration', require('./exploration'));
+
 module.exports = router;
