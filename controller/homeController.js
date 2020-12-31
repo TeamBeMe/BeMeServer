9ad42c1279704e15c534a0a45ac5a8f7aab60b22
Jihyun247 <wlguslove12345@gmail.com>
2020-12-31 22:00:32 +0900

    18-홈화면 답변 불러오기 fixes #18

--- controller/homeController.js
+++ controller/homeController.js
@@ -14,10 +14,10 @@ module.exports = {
         try {
             const page = req.params.page;
             const limit = 5;
-            const answerData = await homeService.getUserAnswersByPage(page, limit);
+            const data = await homeService.getUserAnswersByPage(page, limit);
 
-            console.log(message.GET_ANSWER_SUCCESS)
-            res.status(code.OK).send(util.success(code.OK, message.GET_ANSWER_SUCCESS, answerData));
+            console.log(message.GET_ANSWER_SUCCESS);
+            res.status(code.OK).send(util.success(code.OK, message.GET_ANSWER_SUCCESS, data));
 
         } catch (err) {
             console.error(err);
--- service/homeService.js
+++ service/homeService.js
@@ -19,18 +19,9 @@ module.exports = {
                     attributes: ['id', 'title']
                 }],
                 attributes: ['id', 'answer_idx', 'content', 'created_at', 'answer_date'],
-                raw: true,
             })
 
-            /*const result = await Question.findAll({
-                offset: offset,
-                limit: limit,
-                where: {
-                    id: Answer.question_id, category_id: Category.category_id
-                },
-                attributes: ['id', 'category_name', 'answer_idx', 'question_title', 'content', 'created_at', 'answer_date']
-            })*/
-            return result;
+            return result
 
         } catch (error) {
             throw error;
