9202f0df68bd06ab065b2db0eb9fe8acbe7eb89b
Jihyun247 <wlguslove12345@gmail.com>
2021-01-01 20:25:59 +0900

    33-home 답변 삭제 close #33

--- modules/responseMessage.js
+++ modules/responseMessage.js
@@ -49,10 +49,14 @@ module.exports = {
     UPDATE_PUBLICFLAG_SUCCESS: "공개여부 수정 성공",
     UPDATE_PUBLICFLAG_FAIL: "공개여부 수정 실패",
 
-    /** 댓글 */
+    /* 댓글 */
     POST_COMMENT_SUCCESS: "댓글 생성하기 성공",
     POST_COMMENT_BLOCKED : "댓글이 허용되지 않은 답변입니다",
     INVALID_PARENT_ID : '잘못된 parent_id 입니다',
+
+    /* 답변 삭제 */
+    DELETE_ANSWER_SUCCESS: "답변 삭제 성공",
+    DELETE_ANSWER_FAIL: "답변 삭제 실패",
 
     /* 서버에러 */
     INTERNAL_SERVER_ERROR: "서버 내부 오류",
--- service/homeService.js
+++ service/homeService.js
@@ -1,7 +1,7 @@
 const { Answer, Question, Category } = require('../models');
 
 module.exports = {
-    getUserAnswersByPage : async(page, limit) => {
+    getUserAnswersByPage : async(user_id, page, limit) => {
         //const {id: question_id, title: question_title} = Question;
         //const {id: category_id, name: category_name} = Category;
         try {
@@ -19,6 +19,9 @@ module.exports = {
                     attributes: ['id', 'title']
                 }],
                 attributes: ['id', 'answer_idx', 'content', 'created_at', 'answer_date'],
+                where: {
+                    user_id: user_id
+                }
             })
 
             return result
@@ -26,5 +29,28 @@ module.exports = {
         } catch (error) {
             throw error;
         }
+    },
+
+    checkExiAnswerAndUser : async(answer_id, user_id) => {
+        try {
+            
+            const answer = Answer.findByPk(answer_id);
+            // 존재하는 answer인지 확인
+            if (! answer) {
+                return res.status(code.BAD_REQUEST).send(util.fail(code.BAD_REQUEST, message.INVALID_ANSWER_ID));
+            }
+
+            // 불러온 answer의 유저 id와, 토큰 유저 id가 일치하는 지 확인
+            if (answer.user_id != user_id) {
+                return res.status(code.BAD_REQUEST).send(util.fail(code.BAD_REQUEST, message.INVALID_ANSWER_ID));
+            }
+
+            return answer
+
+        } catch (error) {
+            throw error;
+        }
+
+
     }
 }
