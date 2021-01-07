043917b12066ec3a2106f41ab545528cbfa101ff
Jihyun247 <wlguslove12345@gmail.com>
2021-01-07 15:47:00 +0900

    105-오늘의 질문인지

--- controller/homeController.js
+++ controller/homeController.js
@@ -120,7 +120,7 @@ module.exports = {
             const answerWithIsToday = await homeService.isToday(answer);
 
             console.log(message.GET_QUESTION_SUCCESS);
-            res.status(code.OK).send(util.success(code.OK, message.GET_QUESTION_SUCCESS, answer));
+            res.status(code.OK).send(util.success(code.OK, message.GET_QUESTION_SUCCESS, answerWithIsToday));
 
         } catch (err) {
             console.error(err);
