48a3c16e240e593d1d4e03b18b603fd73a23baa2
Jihyun247 <wlguslove12345@gmail.com>
2021-02-04 11:45:55 +0900

    s3 storage name change

--- modules/multer.js
+++ modules/multer.js
@@ -7,7 +7,7 @@ const s3 = new aws.S3();
 const upload = multer({
     storage: multerS3({
         s3,
-        bucket: '2020-sopt-27',
+        bucket: 'beme-bucket',
         acl: 'public-read',
         key: function(req, file, cb){
             cb(null, 'images/origin/' + Date.now() + '.' + file.originalname.split('.').pop()); // 이름 설정
